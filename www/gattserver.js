765a2a558558c37bec509bef1a9ffcd0a1dedd1a
Sensible-Solutions <Sensible-Solutions@users.noreply.github.com>
2015-09-30 15:09:57 +0200

    Update gattserver.js

--- www/gattserver.js
+++ www/gattserver.js
@@ -2,6 +2,9 @@ var gattServerName = "GattServerPlugin";
 var gattserver = {
 	startServer: function(successCallback, errorCallback, params) {
 		cordova.exec(successCallback, errorCallback, gattServerName, "startServer", [params]); 
+	},
+	alarm: function(successCallback, errorCallback, params) {
+		cordova.exec(successCallback, errorCallback, gattServerName, "alarm", [params]); 
 	}
 }
 module.exports = gattserver;
