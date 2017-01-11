10fde19e91ad24239caa9d70b252da0d02dff7b0
Sensible-Solutions <Sensible-Solutions@users.noreply.github.com>
2017-01-11 10:51:48 +0100

    Update GattServerPlugin.h

--- src/ios/GattServerPlugin.h
+++ src/ios/GattServerPlugin.h
@@ -33,7 +33,8 @@
 }
 
 - (void)startServer:(CDVInvokedUrlCommand *)command;
-- (void)alarm:(CDVInvokedUrlCommand *)command;
+//- (void)alarm:(CDVInvokedUrlCommand *)command;		// Removed 2017-01-10
+- (void)alarm;							// Added 2017-01-10
 - (void)registerNotifications:(CDVInvokedUrlCommand *)command;
 - (void)setAlarmSettings:(CDVInvokedUrlCommand *)command;
 - (void)getAlarmSettings:(CDVInvokedUrlCommand *)command;
