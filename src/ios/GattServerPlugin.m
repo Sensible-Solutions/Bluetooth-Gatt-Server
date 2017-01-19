c0af2679fef52fe100bef5052bc401dc86126bea
Sensible-Solutions <Sensible-Solutions@users.noreply.github.com>
2017-01-19 15:49:22 +0100

    Update GattServerPlugin.m

--- src/ios/GattServerPlugin.m
+++ src/ios/GattServerPlugin.m
@@ -350,11 +350,11 @@ NSString *const KEY_LOG_SETTING = @"log";
 // Sets the application badge number
 - (void)setApplicationBadgeNumber:(CDVInvokedUrlCommand *)command	// Function added 2017-01-19
 {
-    	NSInteger myNumber = [command.arguments objectAtIndex:0];
+    	int myNumber = [command.arguments objectAtIndex:0];
 	[[UIApplication sharedApplication] setApplicationIconBadgeNumber:myNumber];	// Also clears the notifications
 	
 	//UIAlertView *debugMessage = [[UIAlertView alloc] initWithTitle: @"Debug SSNM" message:@"setApplicationBadgeNumber called!" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
-	UIAlertView *debugMessage = [[UIAlertView alloc] initWithTitle: @"Debug SSNMM" message:myNumber delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil]; 
+	UIAlertView *debugMessage = [[UIAlertView alloc] initWithTitle: @"Debug SSNMM" message:[@(myInt) stringValue] delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil]; 
 	[debugMessage show];
 	
 	CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];		// Added 2017-01-19
